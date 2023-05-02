f095fca259a21e831682702e5bb2dd2b2c0af0f0
gaspard.beunier <77894810+1m0ut0n@users.noreply.github.com>
2023-05-02 15:08:30 +0200

    Update views.py

--- server/app/views.py
+++ server/app/views.py
@@ -6,6 +6,7 @@ recu = "couuucouuuu"
 @app.route("/test", methods=["GET", "POST"])
 def test() :
 	if request.method == "POST":
-		output = request.get_json(force=True) #le force=True sert a skip la verification du contentType application/json
-		recu = output['text']
+		#output = request.get_json(force=True) #le force=True sert a skip la verification du contentType application/json
+		#recu = output['text']
+		recu = request.data
 	return render_template("public/index.html", test=recu)
