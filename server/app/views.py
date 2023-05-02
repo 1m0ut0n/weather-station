94ff4adbc3325b384fd1f4687fafed97f9253395
gaspard.beunier <77894810+1m0ut0n@users.noreply.github.com>
2023-05-02 14:06:38 +0200

    change recu

--- server/app/views.py
+++ server/app/views.py
@@ -1,9 +1,10 @@
 from app import app
 from flask import render_template, request, redirect, url_for
 
+recu = "couuucouuuu"
+
 @app.route("/", methods=["GET", "POST"])
 def index() :
-	recu = "couuucouuuu"
 	if request.method == "POST":
 		output = request.get_json(force=True) #le force=True sert a skip la verification du contentType application/json
 		recu = output['text']
