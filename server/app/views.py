3f96027efee18b26eef6f1911780408b2bd777d7
gaspard.beunier <77894810+1m0ut0n@users.noreply.github.com>
2023-05-02 15:10:16 +0200

    Update views.py

--- server/app/views.py
+++ server/app/views.py
@@ -3,8 +3,8 @@ from flask import render_template, request, redirect, url_for
 
 recu = "couuucouuuu"
 
-@app.route("/test", methods=["GET", "POST"])
-def test() :
+@app.route("/", methods=["GET", "POST"])
+def index() :
 	if request.method == "POST":
 		#output = request.get_json(force=True) #le force=True sert a skip la verification du contentType application/json
 		#recu = output['text']
