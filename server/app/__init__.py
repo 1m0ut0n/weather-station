033454880644b9dc07f85c93db9e2040eaee97b9
gaspard.beunier <77894810+1m0ut0n@users.noreply.github.com>
2023-05-02 13:10:55 +0200

    publish branch

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/data.sqlite3.db'
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contact.sqlite3.db'
 
 from app import views
--- server/app/views.py
+++ server/app/views.py
@@ -1,3 +1,10 @@
 from app import app
 from flask import render_template, request, redirect, url_for
 
+@app.route("/", methods=["GET", "POST"])
+def index() :
+	recu = "couuucouuuu"
+	if request.method == "POST":
+		output = request.get_json(force=True) #le force=True sert a skip la verification du contentType application/json
+		recu = output['text']
+	return render_template("public/index.html", test=recu)
